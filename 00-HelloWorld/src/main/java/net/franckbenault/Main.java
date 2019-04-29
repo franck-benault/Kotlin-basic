7dec351b1bc8ac5c860f41b793ee6c694f4266c0
franck-benault <franck-benault@orange.fr>
2019-04-29 23:01:12 +0200

    [EXAMPLE-00] add calling from java

---
+++ 00-HelloWorld/src/main/java/net/franckbenault/Main.java
@@ -0,0 +1,8 @@
+package net.franckbenault;
+
+public class Main {
+
+    public static void main(String [] args) {
+        MainKt.main(args);
+    }
+}
--- 00-HelloWorld/src/main/kotlin/net/franckbenault/main.kt
+++ 00-HelloWorld/src/main/kotlin/net/franckbenault/main.kt
@@ -1,5 +1,5 @@
 package net.franckbenault
 
 fun main(args: Array<String>) {
-    println("HelloWorld")
+    println("HelloWorld from Kotlin")
 }
